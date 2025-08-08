105c11f251d5ebd7527d9fbce4187496addfa05e
Your NameRyan <inoober2008@gmail.com>
2025-08-07 23:03:32 -0400

    BRO HOW??

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   theme: {
     extend: {
       colors: {
@@ -12,3 +12,5 @@ export default {
   },
   plugins: [],
 };
+
+export default config;
